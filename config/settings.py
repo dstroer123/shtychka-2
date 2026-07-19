f6b5aefa985ae898bda9e48cf231db2928687c1a
Andrey <andrejmuzykin63@gmail.com>
2026-07-19 23:24:35 +0600

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -128,12 +128,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-LOGIN_URL = "/login/"
-
-LOGIN_REDIRECT_URL = "/"
-
-LOGOUT_REDIRECT_URL = "/"
-
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
